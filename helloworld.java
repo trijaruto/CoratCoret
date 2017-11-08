aa806fd7c078cea218e5f593e2f1481b5d47fbde
trijaruto <tri.jaruto@smartfren.com>
2017-11-08 11:34:23 +0700

    ant junit commit

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@ public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-		System.out.println("Jenkins Build Test HelloWorld");
+		System.out.println("ant Build Test HelloWorld");
     }
 
 }
---
+++ src/ant_helloworld/ant_helloworld.java
@@ -0,0 +1,7 @@
+package ant_helloworld;
+public class ant_helloworld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window with ant.        
+		System.out.println("ant Build Test HelloWorld");
+    }
+}
